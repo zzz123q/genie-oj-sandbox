63d25a438889c08bc2ec25dc4b607cf27e0a2c4d
zzz123q <1352929539@qq.com>
2024-09-01 15:47:25 +0800

    创建Docker Demo

---
+++ src/main/java/com/zzz123q/genieojsandbox/docker/DockerDemo.java
@@ -0,0 +1,10 @@
+package com.zzz123q.genieojsandbox.docker;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.core.DockerClientBuilder;
+
+public class DockerDemo {
+    public static void main(String[] args) {
+        DockerClient dockerClient = DockerClientBuilder.getInstance().build();
+    }
+}
