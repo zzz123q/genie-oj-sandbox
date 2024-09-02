dac80920ecebb0cd412e93d2037f36ab3d316ff4
zzz123q <1352929539@qq.com>
2024-09-02 21:01:26 +0800

    添加鉴权请求头和密钥

--- src/main/java/com/zzz123q/genieojsandbox/controller/MainController.java
+++ src/main/java/com/zzz123q/genieojsandbox/controller/MainController.java
@@ -1,6 +1,8 @@
 package com.zzz123q.genieojsandbox.controller;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +19,11 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class MainController {
 
+    // 定义鉴权请求头和密钥
+    private static final String AUTH_REQUEST_HEADER = "auth";
+
+    private static final String AUTH_REQUEST_SECRET = "secret";
+
     @Resource(name = "javaNativeCodeSandbox")
     private CodeSandbox codeSandbox;
 
@@ -32,7 +39,14 @@ public class MainController {
      * @return
      */
     @PostMapping("/executeCode")
-    public ExecuteCodeResponse executeCode(@RequestBody ExecuteCodeRequest executeCodeRequest) {
+    public ExecuteCodeResponse executeCode(@RequestBody ExecuteCodeRequest executeCodeRequest,
+            HttpServletRequest request, HttpServletResponse response) {
+        // 进行基本的认证
+        String authHeader = request.getHeader(AUTH_REQUEST_HEADER);
+        if (!AUTH_REQUEST_SECRET.equals(authHeader)) {
+            response.setStatus(403);
+            return null;
+        }
         if (executeCodeRequest == null) {
             throw new RuntimeException("请求参数为空");
         }
